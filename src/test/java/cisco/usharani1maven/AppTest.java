256ed8e54b6b833e19026e6f49e0ebb8ceca2283
usharani12345678910 <rani.bhoomula@gmail.com>
2022-05-31 09:39:50 +0530

    this usha maven

---
+++ src/test/java/cisco/usharani1maven/AppTest.java
@@ -0,0 +1,20 @@
+package cisco.usharani1maven;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
